41a983eaf91544993ee29b789659776443f2a5c9
Alexander Schelchere <ascellie@gmail.com>
2019-05-25 15:10:34 -0400

    Help helps

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,8 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  # list of commands
+  comm_list = ["help", "list", "play", "exit"]
+  puts "The available commands are: \n #{comm_list.map{|com| puts com}}"
+end
