d0a41bab7536a1cf08831023f61e818549b77e4c
Alexander Schelchere <ascellie@gmail.com>
2019-05-26 18:53:58 -0400

    play - the struggle is... weird?

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,19 +13,19 @@ songs = [
 ]
 
 # get the songs haha
-def playlist 
-  songs = [
-    "Phoenix - 1901",
-    "Tokyo Police Club - Wait Up",
-    "Sufjan Stevens - Too Much",
-    "The Naked and the Famous - Young Blood",
-    "(Far From) Home - Tiga",
-    "The Cults - Abducted",
-    "Phoenix - Consolation Prizes",
-    "Harry Chapin - Cats in the Cradle",
-    "Amos Lee - Keep It Loose, Keep It Tight"
-  ]
-end
+# def playlist 
+#   songs = [
+#     "Phoenix - 1901",
+#     "Tokyo Police Club - Wait Up",
+#     "Sufjan Stevens - Too Much",
+#     "The Naked and the Famous - Young Blood",
+#     "(Far From) Home - Tiga",
+#     "The Cults - Abducted",
+#     "Phoenix - Consolation Prizes",
+#     "Harry Chapin - Cats in the Cradle",
+#     "Amos Lee - Keep It Loose, Keep It Tight"
+#   ]
+# end
 
 def help
   # list of commands
@@ -37,24 +37,40 @@ def list(songs)
   songs.map{ |song| puts song }
 end
 
-# def play(entry)
-#   # error message
-#   err_message = "Invalid input, please try again"
-#   # successful song pick
-#   song_entry = ""
-#   return puts err_message if entry == "" || !entry.is_a?(String) && !entry.is_a?(Integer) # || playlist.find{|song| song.include?(entry)} == nil
-#   # go through our playlist
-#   if entry.is_a?(Integer)
-#     song_entry = playlist[entry - 1] 
-#   else 
-#     song_entry = playlist.find{|song| song.include?(entry)}
-#   end
+def play(entry)
+  songs = [
+    "Phoenix - 1901",
+    "Tokyo Police Club - Wait Up",
+    "Sufjan Stevens - Too Much",
+    "The Naked and the Famous - Young Blood",
+    "(Far From) Home - Tiga",
+    "The Cults - Abducted",
+    "Phoenix - Consolation Prizes",
+    "Harry Chapin - Cats in the Cradle",
+    "Amos Lee - Keep It Loose, Keep It Tight"
+  ]
+
+  # error message
+  err_message = "Invalid input, please try again"
+  # successful song pick
+  # song_entry = ""
+  # return puts err_message if entry == "" || entry == nil
+  return puts err_message if !entry.is_a?(String) & !entry.is_a?(Numeric) # || playlist.find{|song| song.include?(entry)} == nil
+  # go through our playlist
+  if entry.is_a?(Numeric)
+    puts songs[entry - 1] 
+    return songs[entry - 1]
+  elsif entry.is_a?(String)
+    song_entry = songs.select{|song| song.include?(entry)}
+    puts song_entry
+    return song_entry
+  end
   
-#   puts song_entry
-#   return song_entry
-#   # record = playlist.find{|song| song.include?("jackson")}
-#   # puts "fail" if record == nil
-# end
+  # puts song_entry
+  # return song_entry
+  # record = playlist.find{|song| song.include?("jackson")}
+  # puts "fail" if record == nil
+end
 
 
 def exit_jukebox
