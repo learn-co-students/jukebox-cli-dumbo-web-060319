cd170ecd4f27b1f0ada7a88f1f4ce0d724938227
Alexander Schelchere <ascellie@gmail.com>
2019-05-26 15:45:03 -0400

    list - complete

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,16 +12,46 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+# get the songs haha
+def playlist 
+  songs = [
+    "Phoenix - 1901",
+    "Tokyo Police Club - Wait Up",
+    "Sufjan Stevens - Too Much",
+    "The Naked and the Famous - Young Blood",
+    "(Far From) Home - Tiga",
+    "The Cults - Abducted",
+    "Phoenix - Consolation Prizes",
+    "Harry Chapin - Cats in the Cradle",
+    "Amos Lee - Keep It Loose, Keep It Tight"
+  ]
+end
+
 def help
   # list of commands
   comm_list = ["help", "list", "play", "exit"]
   puts "The available commands are: \n #{comm_list.map{|com| puts com}}"
 end
 
-def play(entry)
-  puts songs
-end
+# def play(entry)
+#   # error message
+#   err_message = "Invalid input, please try again"
+#   # successful song pick
+#   song_entry = ""
+#   return puts err_message if entry == "" || !entry.is_a?(String) && !entry.is_a?(Integer) # || playlist.find{|song| song.include?(entry)} == nil
+#   # go through our playlist
+#   if entry.is_a?(Integer)
+#     song_entry = playlist[entry - 1] 
+#   else 
+#     song_entry = playlist.find{|song| song.include?(entry)}
+#   end
+  
+#   puts song_entry
+#   return song_entry
+#   # record = playlist.find{|song| song.include?("jackson")}
+#   # puts "fail" if record == nil
+# end
 
-# For method, play, I get an error message:
-# undefined local variable or method `songs'
-# even though it's globally defined on line 3
+def list(songs)
+  songs.map{ |song| puts song }
+end
