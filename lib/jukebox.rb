eaa654e45b411ab7ddf3d1f090d35d2fb0e53ed4
github username <github email address>
2019-05-26 21:52:22 -0400

    used system open to open .mp3

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,54 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help 
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+
+def list(my_songs)
+  my_songs.each_with_index do |name,i|
+    puts (i+1).to_s+ ". "+name 
+  end
+end
+
+def play(my_songs)
+  puts "Please enter a song name or number:"
+  input=gets.chomp
+  if my_songs.include?(input) || (1..9).include?(input.to_i)
+    if (1..9).include?(input.to_i)
+      input=input.to_i
+      puts "Playing <#{my_songs[input-1]}>"
+    else
+      puts "Playing <#{input}>"
+    end
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+puts "Goodbye"
+end
+
+def run(song)
+help
+command=""
+  until command== "exit"
+  puts "Please enter a command:"
+  command=gets.chomp
+      if command == "list"
+          list(song)
+      elsif command== "play"
+          play(song)
+      elsif command =="help"
+          help
+  
+      end
+  end
+  exit_jukebox
+end
